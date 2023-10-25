15491726fb84e7c2f4e04c939f04bc1d60045f2e
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-10-25 09:15:17 -0500

    list updated

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -2,12 +2,17 @@ import { RenderDataGrid } from "../../Atom/RenderDataGrid/RenderDataGrid";
 import { useReservations } from "../../context/ContextData";
 import { IconButton, Avatar } from "@material-ui/core";
 import DeleteIcon from "@mui/icons-material/Delete";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { Tooltip } from "@mui/material";
+import { useState } from "react";
 
 {
   /* debo hacerlo condicional si no hay elementos añadidos en la lista de las propiedad enviadas  */
 }
 export const List = (): JSX.Element => {
   const { reservations, setReservations } = useReservations();
+
+  const [show, setShow] = useState<boolean>(false);
 
   const handleDeleteElement = (params) => {
     const newReservations = reservations.filter(
@@ -51,13 +56,52 @@ export const List = (): JSX.Element => {
               />
             </Avatar>
           </IconButton>
+          <Tooltip
+            title={
+              show ? (
+                <>
+                  <p>Origen: {params.row.origin}</p>
+                  <p>Destino: {params.row.destination}</p>
+                  <p>Pasajeros: {params.row.passengers}</p>
+                  <p>Fecha: {params.row.date}</p>
+                  <p>Hora: {params.row.time}</p>
+                </>
+              ) : null
+            }
+            placement="top"
+            arrow
+          >
+            <IconButton
+              aria-label="show"
+              onMouseOver={() => {
+                setShow(true);
+              }}
+            >
+              <Avatar
+                style={{
+                  width: 24,
+                  height: 24,
+                  background: "#fff",
+                  border: "2px solid",
+                }}
+                variant="rounded"
+              >
+                <VisibilityIcon
+                  sx={{
+                    color: "primary.main",
+                    width: "18px",
+                    height: "18px",
+                  }}
+                />
+              </Avatar>
+            </IconButton>
+          </Tooltip>
         </>
       ),
     },
   ];
 
-
-  if(!reservations.length) return (<></>)
+  if (!reservations.length) return <></>;
 
   return (
     <>
